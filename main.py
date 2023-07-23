638d86e6aacc617ba145e966e445bbc0d0adf9fa
Matthieu <matthieu.felten22@gmail.com>
2023-07-23 11:55:12 +0200

    Update

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from googlesearch import search
 
 load_dotenv('.env')
 
-intents = discord.Intents().all()
-client = discord.Client(intents=intents)
-
 class NombreMistere:
     def __init__(self, id_user, id_message, channel):
         self.id_user = id_user
@@ -50,23 +47,14 @@ class NombreMistere:
 
 class MatBot(commands.Bot):
     def __init__(self) -> None:
-        super().__init__(command_prefix="!", intents=intents)
-        self.rep = {"bonjour": ["Bien le boujour !", "Salut !", "Coucou", "Bonjour à toi aussi !", "Salut", "Hey !", "Hello"]}
+        super().__init__(command_prefix="!", intents=discord.Intents.all(), description="Ceci est un bot test créé par MAT06mat !")
+        self.rep = {"Bonjour": ["Bien le boujour !", "Salut !", "Coucou", "Bonjour à toi aussi !", "Salut", "Hey !", "Hello"]}
         self.jeu = []
         self.admin = [861240797659136012]
         self.ban = [] # [1072216355757113384]
         self.pronom = ["Je", "Tu", "Il"]
         self.verbe = ["mange", "fais", "roule", "regarde", "ajoute"]
         self.gn = ["de la nourriture", "du caca", "sur la route", "la télé", "des chiffres"]
-
-    def get_args(self, message):
-        ls = message.content.split(' ')
-        m = ''
-        for i in ls:
-            if i == ls[0]:
-                continue
-            m = m + ' ' + i
-        return m
     
     async def on_ready(self):
         print(f"{self.user.display_name} est connecté au serveur.")    
@@ -77,78 +65,93 @@ class MatBot(commands.Bot):
             # await self.get_channel(1132312138980012135).send(message.author.name+": "+message.content)
         
         if message.author.id == 1069896287765401630:
-            return
+            return await super().on_message(message)
         elif message.author.id in self.ban:
             await message.delete()
+            return await super().on_message(message)
         else:
             for j in self.jeu:
                 if j.id_user == message.author.id and message.channel == j.channel:
                     if await j.message(message):
                         self.jeu.remove(j)
-                    return
+                    return await super().on_message(message)
             match message.content.lower().split(' ')[0]:
-                case 'bonjour' | 'salut' | 'hey' | 'slt' | 'bjr' | "cc" | "coucou" | "bonsoir" | "bonchoir":
+                case 'bonjour' | 'salut' | 'hey' | 'slt' | 'bjr' | "cc" | "coucou" | "bonsoir" | "bonchoir" | "bienvenue":
                     await message.add_reaction("👋")
-                    await message.channel.send(self.rep["bonjour"][random.randint(0, len(self.rep["bonjour"]))])
-                case '/jouer':
-                    new_message = await message.channel.send("Voulez vous jouer au nombre mistère ?")
-                    await new_message.add_reaction("✅")
-                    await new_message.add_reaction("❌")
-                    self.jeu.append(NombreMistere(id_user=message.author.id, id_message=new_message.id, channel=message.channel))
-                case '/sondage':
-                    m = self.get_args(message=message)
-                    if m != "":
-                        new_message = await message.channel.send(m)
-                    await new_message.add_reaction("✅")
-                    await new_message.add_reaction("🔸")
-                    await new_message.add_reaction("❌")
-                    await message.delete()
-                case '/repond' | '/répond':
-                    m = self.get_args(message=message)
-                    if m != "":
-                        new_message = await message.channel.send(m)
-                    await message.delete()
-                case '/clear':
-                    if not message.author.id in self.admin:
-                        await message.channel.send("Vous n'avez pas assez de droits pour effectuer cette commande.")
-                        return
-                    try:
-                        m = self.get_args(message=message)
-                        m_int = int(m)
-                        mess = message.channel.history(limit=m_int+1)
-                        async for ms in mess:
-                            await ms.delete()
-                    except:
-                        await message.channel.send("Arguments incorrecte.")
-                case "/search":
-                    m = self.get_args(message=message)
-                    await message.channel.send(f"Voici ce que j'ai trouvé :")
-                    for lien in search(m, num_results=3, lang="fr", timeout=2):
-                        await message.channel.send(f" - {lien}")
-                case "/alea" | "/aléa":
-                    await message.channel.send(f"{random.choice(self.pronom)} {random.choice(self.verbe)} {random.choice(self.gn)}.")
-                case "/pronom":
-                    m = self.get_args(message=message)
-                    self.pronom.append(m)
-                    await message.channel.send(f"Le pronom '{m}' à bien été ajouté !")
-                case "/gn":
-                    m = self.get_args(message=message)
-                    self.gn.append(m)
-                    await message.channel.send(f"Le groupe nominal '{m}' à bien été ajouté !")
-                case "/verbe":
-                    m = self.get_args(message=message)
-                    self.verbe.append(m)
-                    await message.channel.send(f"Le verbe '{m}' à bien été ajouté !")
-                    
-    
+                    await message.channel.send(random.choice(self.rep["Bonjour"]))
+        return await super().on_message(message)
+                
 
 bot = MatBot()
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    for j in bot.jeu:
-        if j.id_user == user.id and j.id_message == reaction.message.id and reaction.message.channel == j.channel:
-            if await j.start(reaction):
-                bot.jeu.remove(j)
+    for jeu in bot.jeu:
+        if jeu.id_user == user.id and jeu.id_message == reaction.message.id and reaction.message.channel.id == jeu.channel.id:
+            if await jeu.start(reaction):
+                bot.jeu.remove(jeu)
+
+@bot.command(name="repond")
+async def test(ctx, arg):
+    await ctx.message.delete()
+    await ctx.send(arg)
+
+@bot.command(name="jouer")
+async def jouer(ctx):
+    new_message = await ctx.send("Voulez vous jouer au nombre mistère ?")
+    await new_message.add_reaction("✅")
+    await new_message.add_reaction("❌")
+    bot.jeu.append(NombreMistere(id_user=ctx.message.author.id, id_message=new_message.id, channel=ctx.channel))
+
+@bot.command(name="sondage")
+async def sondage(ctx, *args):
+    await ctx.message.delete()
+    new_message = await ctx.send("**Nouveau Sondage :**\n"+" ".join(args))
+    await new_message.add_reaction("✅")
+    await new_message.add_reaction("🔸")
+    await new_message.add_reaction("❌")
+
+@bot.command(name="research")
+async def research(ctx, num_results, *args):
+    try:
+        num_results = int(num_results)
+    except:
+        await ctx.send(f"ERROR: {bot.command_prefix}research [nombre_de_résultats] [votre_recherche]")
+        return
+    await ctx.send(f"Voici ce que j'ai trouvé :")
+    for lien in search(" ".join(args), num_results=num_results, lang="fr", timeout=2):
+        await ctx.send(f" - {lien}")
+
+@bot.command(name="clear")
+async def clear(ctx, nb):
+    if not ctx.message.author.id in bot.admin:
+        await ctx.send("Vous n'avez pas assez de droits pour effectuer cette commande.")
+        return
+    try:
+        nb = int(nb)
+        messages = ctx.history(limit=nb+1)
+        async for message in messages:
+            await message.delete()
+    except:
+        await ctx.send(f"ERROR: {bot.command_prefix}clear [nombre_de_messages_à_effacer]")
+
+@bot.command(name="alea")
+async def alea(ctx):
+    await ctx.send(f"{random.choice(bot.pronom)} {random.choice(bot.verbe)} {random.choice(bot.gn)}.")
+
+@bot.command(name="pronom")
+async def pronom(ctx, pronom):
+    bot.pronom.append(pronom)
+    await ctx.send(f"Le pronom '{pronom}' à bien été ajouté !")
+
+@bot.command(name="verbe")
+async def pronom(ctx, verbe):
+    bot.verbe.append(verbe)
+    await ctx.send(f"Le verbe '{verbe}' à bien été ajouté !")
+
+@bot.command(name="gn")
+async def pronom(ctx, gn):
+    bot.gn.append(gn)
+    await ctx.send(f"Le gn '{gn}' à bien été ajouté !")
 
 bot.run(os.environ.get("TOKEN"))
